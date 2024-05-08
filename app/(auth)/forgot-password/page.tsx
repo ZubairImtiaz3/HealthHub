e21475693bb78b2d361041f6d55f9975eafd66eb
Zubair Imtiaz <106450046+ZubairImtiaz3@users.noreply.github.com>
2024-05-09 04:36:54 +0500

    added forgot pass page

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,12 @@
+import { ForgotPassForm } from "@/components/ForgotPassForm";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <ForgotPassForm />
+    </div>
+  );
+};
+
+export default page;
--- components/ForgotPassForm.tsx
+++ components/ForgotPassForm.tsx
@@ -5,15 +5,15 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function LoginForm() {
+export function ForgotPassForm() {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[400px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
+            <h1 className="text-3xl font-bold">Forgot Password?</h1>
             <p className="text-balance text-muted-foreground">
-              Enter your credentials below to login to your account
+              Enter your email below to reset your password
             </p>
           </div>
           <div className="grid gap-4">
@@ -26,30 +26,15 @@ export function LoginForm() {
                 required
               />
             </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
-            </div>
             <Button type="submit" className="w-full">
-              Login
+              Reset Password
             </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="/signup" className="underline">
-              Sign up
-            </Link>
+            <div className="mt-4 text-center text-sm">
+              Remember your password?{" "}
+              <Link href="/login" className="underline">
+                Sign in
+              </Link>
+            </div>
           </div>
         </div>
       </div>
