aa2bbf5deb3a6f9281ae7c8e055df188f59e5211
Zubair Imtiaz <106450046+ZubairImtiaz3@users.noreply.github.com>
2024-07-06 17:35:53 +0500

    Added patients with admin

--- app/(dashboard)/user-panel/page.tsx
+++ app/(dashboard)/user-panel/page.tsx
@@ -1,6 +1,3 @@
-import { File } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import PatientTabContent from "@/components/dashboard/PatientTabContent";
 import UserProfile from "@/components/dashboard/UserProfile";
 
@@ -8,22 +5,7 @@ export default function page() {
   return (
     <>
       <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-        <Tabs defaultValue="week">
-          <div className="flex items-center">
-            <TabsList>
-              <TabsTrigger value="week">Week</TabsTrigger>
-              <TabsTrigger value="month">Month</TabsTrigger>
-              <TabsTrigger value="year">Year</TabsTrigger>
-            </TabsList>
-            <div className="ml-auto flex items-center gap-2">
-              <Button size="sm" variant="outline" className="h-7 gap-1 text-sm">
-                <File className="h-3.5 w-3.5" />
-                <span className="sr-only sm:not-sr-only">Export</span>
-              </Button>
-            </div>
-          </div>
-          <PatientTabContent />
-        </Tabs>
+        <PatientTabContent />
       </div>
       <div>
         <UserProfile />
--- components/dashboard/PatientTabContent.tsx
+++ components/dashboard/PatientTabContent.tsx
@@ -14,7 +14,11 @@ const PatientTabContent = async () => {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
 
-  const { data: patients, error } = await supabase.from("patients").select("*");
+  const { data: patients, error } = await supabase.from("patients").select(
+    `*, 
+        admins (*, profiles (*))
+        `,
+  );
 
   if (error) {
     console.error("Error fetching patients:", error);
@@ -22,44 +26,47 @@ const PatientTabContent = async () => {
   }
 
   return (
-    <TabsContent value="week">
-      <Card x-chunk="dashboard-05-chunk-3">
-        <CardHeader className="px-7">
-          <CardTitle>Patients</CardTitle>
-          <CardDescription>
-            You can view all associated patients here.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col gap-6">
-            {patients.length > 0 ? (
-              patients.map(patient => (
-                <div key={patient.id}>
-                  <Card>
-                    <CardHeader className="flex items-center justify-between">
-                      <div>
-                        <h3 className="text-lg font-medium">
-                          {patient.first_name} {patient.last_name}
-                        </h3>
-                        <p className="text-sm text-gray-500 dark:text-gray-400">
-                          Dated:{" "}
-                          {new Date(patient.created_at).toLocaleDateString()}
-                        </p>
-                      </div>
-                      <ViewReportsBtn id={patient.id} />
-                    </CardHeader>
-                  </Card>
-                </div>
-              ))
-            ) : (
-              <h3 className="text-center font-semibold">
-                No patients record found for you right now.
-              </h3>
-            )}
-          </div>
-        </CardContent>
-      </Card>
-    </TabsContent>
+    <Card className="mt-12" x-chunk="dashboard-05-chunk-3">
+      <CardHeader className="px-7">
+        <CardTitle>Patients</CardTitle>
+        <CardDescription>
+          You can view all associated patients here.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-col gap-6">
+          {patients.length > 0 ? (
+            patients.map(patient => (
+              <div key={patient.id}>
+                <Card>
+                  <CardHeader className="flex items-center justify-between">
+                    <div className="text-center">
+                      <h3 className="text-lg font-medium">
+                        {patient.first_name} {patient.last_name}
+                      </h3>
+                      <p className="text-sm text-gray-500 dark:text-gray-400">
+                        Created By: ({patient.admins.admin_type}){" "}
+                        {patient.admins.profiles.first_name}{" "}
+                        {patient.admins.profiles.last_name}
+                      </p>
+                      <p className="text-sm text-gray-500 dark:text-gray-400">
+                        Dated:{" "}
+                        {new Date(patient.created_at).toLocaleDateString()}
+                      </p>
+                    </div>
+                    <ViewReportsBtn id={patient.id} />
+                  </CardHeader>
+                </Card>
+              </div>
+            ))
+          ) : (
+            <h3 className="text-center font-semibold">
+              No patients record found for you right now.
+            </h3>
+          )}
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
